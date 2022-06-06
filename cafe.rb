d0be8572bebfcf70ea8e0dc7ebafdceae19e3d38
mame0124 <mame0124.rkn@gmail.com>
2022-06-06 14:17:24 +0900

    debug cafe.rb

--- cafe.rb
+++ cafe.rb
@@ -15,8 +15,8 @@ FOODS = [
 ].freeze
 
 def take_order(menus)
-  menus.each.with_index(1) do |menus, i|
-    puts "(#{i})#{menus[:name]}: #{menus[:price]}円"
+  menus.each.with_index(1) do |menu, i|
+    puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
   @order_number = (gets.to_i) - 1
